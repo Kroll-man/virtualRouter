9175d80c00013a3b0317ed4cf17e274331e6b828
Kroll_man <wtwithers2020@gmail.com>
2023-04-05 10:48:14 -0400

    add router record stuff

--- src/setup/RouteRecord.java
+++ src/setup/RouteRecord.java
@@ -4,9 +4,32 @@ import java.io.Serializable;
 
 
 public class RouteRecord implements Serializable {
+
+
     //TODO: This class may be implemented as a "helper class" for helping implement the Table class
     // This class is optional: if you do not need this class, you can keep it empty
-    private int destination;
     private int cost;
-    private int nexthop;
+
+    private int nextHop;
+
+    public RouteRecord(int cst, int nxthop){
+        this.cost = cst;
+        this.nextHop = nxthop;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public int getNextHop() {
+        return nextHop;
+    }
+    public void setCost(int cst) {
+        this.cost = cst;
+    }
+
+    public void setNextHop(int nxthop) {
+        this.nextHop = nxthop;
+    }
+
 }
--- src/setup/Router.java
+++ src/setup/Router.java
@@ -89,7 +89,20 @@ public class Router {
 
     private void initializeTable(List<Link> links) {
 	//TODO: complete this method to initialize the distance vector: _table
-
+        // use JSONTool to get links
+        List<Link> links = _jsonTool.getLinks();
+        //loop though each link
+        for(Link link: links){
+            //Find out ID of neighbor using the link
+            int otherRouterId =  link.connectingRouterId().get(0) == _Id ? link.connectingRouterId().get(1) : link.connectingRouterId().get(0);
+            //add it to the routing table, represented here by member variable _table
+            _table.addEntry(otherRouterId, new RouteRecord(link.weight(), otherRouterId));
+        }
+        // add entry for self, equal to 0
+        _table.addEntry(_Id, new RouteRecord(0, _Id));
+        //print it to console to show intitial table with direct links
+        System.out.println("Initial direct link table");
+        System.out.println(_table);
     }
 
     private void initializeNeighbors(List<Link> links) {
